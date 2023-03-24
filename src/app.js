88f0520ada86a23fc1735bbf1815d4deb543b4f9
tinh.nguyen <tinh.nguyen@napaglobal.com>
2023-03-24 09:41:48 +0700

    init project

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const compression = require("compression");
+const express = require("express");
+const { default: helmet } = require("helmet");
+const app = express();
+const morgan = require("morgan");
+
+// init middleware
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+// init DB
+// init routes
+app.get("/", (req, res, next) => {
+  const strCompress = "Hello Framework Express";
+  return res
+    .status(200)
+    .json({
+      message: "Welcome to Express",
+      metadata: strCompress.repeat(100000),
+    });
+});
+// handling error
+module.exports = app;
